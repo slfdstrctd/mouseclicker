982b07b67efa873e130bdcf6ee61b785c39b5ca8
slfdstrctd <artichek123@gmail.com>
2019-11-23 22:31:43 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,68 @@
+// Params
+let img_count
+let del_count = 0
+let mice_max_width = 120
+const mice_max = 104
+let count = document.getElementById('count')
+
+document.ready = init(null)
+
+document.getElementById("button").onclick = init
+
+function init() {
+	del_count = 0
+	img_count = document.getElementById("input").value % 104 || 10 + getRandomInt(30)
+	count.innerText = del_count + "/" + img_count
+
+	let i = 1
+	let main = document.getElementById('main')
+	let mice = document.getElementById('mice')
+	let loading = document.getElementById('loading')
+
+	while (mice.firstChild) {
+		mice.removeChild(mice.firstChild)
+	}
+
+	while (i <= img_count) {
+		let mice_w = 40 + getRandomInt(mice_max_width)
+		let img = new Image()
+
+		img.src = `./img/${normalize(i)}.png`
+		img.style.position = "absolute"
+		img.style.left = `${getRandomInt(window.innerWidth * 0.7)}px`
+		img.style.top = `${getRandomInt(window.innerHeight * 0.6)}px`
+		img.onload = async function () {
+			let k = this.height / (this.width / mice_w)
+			this.width = mice_w
+			this.height = k
+			this.onclick = removeMice
+		}
+		i++
+		mice.appendChild(img)
+	}
+
+	main.style.display = "block"
+	loading.style.display = "none"
+}
+
+function removeMice(props) {
+	if (!props.target.classList.contains('fade')) {
+		del_count++
+		count.innerText = del_count + "/" + img_count
+	}
+	props.target.classList.add('fade')
+	setTimeout(function () {
+		props.target.remove()
+	}, 500)
+
+}
+
+function getRandomInt(max) {
+	return Math.floor(Math.random() * Math.floor(max))
+}
+
+function normalize(t) {
+	if (t < 10) return "00" + t
+	else if (t < 100) return "0" + t
+	else return t
+}
